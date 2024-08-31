e17edcd87128b90b992e78f96a38b93ae068345d
Abhigyan Tripathi <74041526+abhigyan412@users.noreply.github.com>
2024-08-31 21:16:30 +0530

    commit latest

--- techpath/techpath/settings.py
+++ techpath/techpath/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['travelapi-1.onrender.com']
 
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000"],
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
